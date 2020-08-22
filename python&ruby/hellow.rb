a2f7752cd755be9111c39c5f57bb858f5c86f5e2
hyunbin1 <kimgusqls1@gmail.com>
2020-08-22 10:11:26 +0900

    특수문자

--- python&ruby/hellow.rb
+++ python&ruby/hellow.rb
@@ -1,9 +1,6 @@
 puts('hellow')
 puts('Hello'[2])
 
-puts
-
-
 # 대문자로 바꾸기
 puts('hello world'.capitalize())
 
@@ -15,3 +12,21 @@ puts("hello world".length())
 
 #원하는 문자 바꾸기
 puts("Hello world".sub('world', 'programming'))
+
+#특수문자
+puts("hyunbin's \"tutorial\"")
+
+puts("\\")
+
+#줄바꾸기(\n)
+puts("hello\nworld")
+# 하지만 ''로 사용하면 정보가 그대로 나옴(파이썬과 다른점.)
+puts('hello\nworld')
+
+#tab key(\t)
+puts("Hello\tworld")
+
+#경고음 나오게하기(\a)
+puts("\a")
+
+
--- python&ruby/python.py
+++ python&ruby/python.py
@@ -14,8 +14,19 @@ print("hello world".__len__())
 #2.
 print(len("hello world"))
 
+#원하는 문자로 바꾸기
+print("hello world".replace('world', "pragrammar"))
 
+#특수문자
+print("hyunbin's \"World\"")
 
+print("\\")
 
+#줄바꿈
+print("hello\nworld")
 
+#tab key(\t)
+print("Hello\tworld")
 
+#경고음 나오게 하기
+print("\a")
